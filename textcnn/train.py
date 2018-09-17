9dfe4125acc4d852782c9d11754bcb4b76d58156
Qingchao Kong <kqingchao@gmail.com>
2018-09-17 17:56:06 +0800

    update

--- textcnn/train.py
+++ textcnn/train.py
@@ -20,10 +20,11 @@ tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training d
 tf.flags.DEFINE_string("train_data_file", "../data/cutclean_label_corpus10000.txt", "Data source for the positive data.")
 tf.flags.DEFINE_string("train_label_data_file", "", "Data source for the label data.")
 tf.flags.DEFINE_string("w2v_file", "../data/vectors.bin", "w2v_file path")
+
 # Model Hyperparameters
 tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
 tf.flags.DEFINE_string("filter_sizes", "2,3,4", "Comma-separated filter sizes (default: '3,4,5')")
-tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
+tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)") # depth of per filter size
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
 
@@ -33,6 +34,7 @@ tf.flags.DEFINE_integer("num_epochs", 10, "Number of training epochs (default: 2
 tf.flags.DEFINE_integer("evaluate_every", 50, "Evaluate model on dev set after this many steps (default: 100)")
 tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
 tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
+
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
@@ -43,8 +45,6 @@ print("\nParameters:")
 for attr, value in sorted(FLAGS.__flags.items()):
     print("{}={}".format(attr.upper(), value))
 print("")
-
-
 
 
 def load_data(w2v_model):
